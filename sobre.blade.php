9445100a7d3ca9090ea03a6e08475a122dfe7282
Gabrielescher <127734429+Gabrielescher@users.noreply.github.com>
2024-10-25 12:15:16 -0300

    Add files via upload

---
+++ sobre.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre</title>
+    <link rel="stylesheet" href="/css/estilos.css">
+</head>
+<body>
+    <img src="img/Laravel.png" alt="Deu ruim" >
+   
+                    <h1>O que é Framework Laravel?</h1>
+                    <h3>Ele permite aos desenvolvedores especificar e instalar bibliotecas e pacotes de terceiros em seus projetos.<br>
+                                  Isso simplifica o processo de gerenciamento de dependências, facilitando a inclusão e atualização de bibliotecas externas necessárias para o desenvolvimento de aplicações Laravel.</h3>
+<p>
+                     <h1>Como é usado?</h1>
+                     <h3>Laravel é uma ferramenta poderosa para desenvolvedores PHP que buscam construir aplicações web modernas e eficientes.<br> Sua combinação de simplicidade, robustez e uma comunidade ativa torna-o uma excelente escolha para projetos de todos os tipos.</h3>
+</p>
+                      <h1>O motivo de o usarem?</h1>
+                      <h3>O Laravel é ideal para construir todo tipo de aplicativos web. <br>Funciona sites institucionais, blogs, plataformas de e-commerce e sistemas de gerenciamento de conteúdo (CMS).<br> Ele fornece uma estrutura robusta para lidar com autenticação, manipulação de bancos de dados, gerenciamento de sessões e muito mais</h3>
+<p>
+                <h1>Quando foi criado?</h1>
+                <h3>Como Taylor B. Otwell era um grande fã do CodeIgniter, ele aproveitou a oportunidade para criar um framework mais moderno que teria as características do CodeIgniter. <br>Sendo assim, em 2011 ele criou o Laravel.</h3>
+</p>
+
+
+</body>
+
+
+</html>
